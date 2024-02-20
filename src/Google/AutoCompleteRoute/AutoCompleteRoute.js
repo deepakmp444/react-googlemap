795fb702e937562dfeea8806dc1f1a10b3d86f7b
deepakmp444 <deepakmp444@gmail.com>
2024-02-20 18:19:28 +0530

    direction api changes

--- src/Google/AutoCompleteRoute/AutoCompleteRoute.js
+++ src/Google/AutoCompleteRoute/AutoCompleteRoute.js
@@ -5,31 +5,75 @@ import {
 import { useState } from 'react';
 import Directions from './Directions';
 import AutoCompletes from './AutoCompletes';
+import Select from 'react-select'
+
+
+const transitOptions = [
+  { value: 'TRAIN', label: 'TRAIN' },
+  { value: 'BUS', label: 'BUS' },
+  { value: 'RAIL', label: 'RAIL' },
+  { value: 'SUBWAY', label: 'SUBWAY' },
+  { value: 'TRAM', label: 'TRAM' },
+]
+
+const travelModeOptions = [
+  { value: 'DRIVING', label: 'DRIVING' },
+  { value: 'BICYCLING', label: 'BICYCLING' },
+  { value: 'WALKING', label: 'WALKING' },
+  { value: 'TRANSIT', label: 'TRANSIT' },
+]
 
 
 function AutoCompleteRoute() {
   const [selectedSourcePlace, setSelectedSourcePlace] = useState(null);
   const [selectedDestinationPlace, setSelectedDestinationPlace] = useState(null);
   const [startEndDirection, setStartEndDirection] = useState({
-    origin:"",
-    destination:""
+    origin: "",
+    destination: ""
   });
-  console.log('startEndDirection:', startEndDirection)
+
+  const [selectedTravelMode, setSelectTravelMode] = useState({
+    label: null,
+    value: null
+  })
+
+  const [selectedTransitMode, setSelectTransitMode] = useState({
+    label: null,
+    value: null
+  })
 
   const [routeSummary, setRouteSummary] = useState({})
+  console.log('routeSummary:', routeSummary)
+  // console.log('routeSummaryLegs:', routeSummary?.[0]?.legs?.[0].steps)
 
-  const handleRoute = ()=>{
+  const handleRoute = () => {
+    // setStartEndDirection({
+    //   origin: selectedSourcePlace.formatted_address,
+    //   destination: selectedDestinationPlace.formatted_address
+    // })
     setStartEndDirection({
-      origin: selectedSourcePlace.formatted_address,
-      destination: selectedDestinationPlace.formatted_address
+      origin: "Kahalgaon, Bihar, India",
+      destination: "Patna, Bihar, India"
     })
-    console.log('selectedSourcePlace.formatted_address:', selectedSourcePlace.formatted_address)
-    console.log('selectedDestinationPlace.formatted_address:', selectedDestinationPlace.formatted_address)
+    // console.log('selectedSourcePlace.formatted_address:', selectedSourcePlace.formatted_address)
+    // console.log('selectedDestinationPlace.formatted_address:', selectedDestinationPlace.formatted_address)
 
   }
-  console.log('handleRoute:')
 
-  console.log('duration:', )
+  const handleTravelMode = (value) => {
+    setSelectTravelMode(value)
+  }
+
+  const handleTransitMode = (value) => {
+    if (value) {
+      setSelectTransitMode(value)
+    } else {
+      setSelectTransitMode({
+        label: null,
+        value: null
+      })
+    }
+  }
 
   return (
     <div>
@@ -39,22 +83,64 @@ function AutoCompleteRoute() {
             defaultCenter={{ lat: 20.59, lng: 78.96 }}
             gestureHandling={'greedy'}
             disableDefaultUI={true} mapId={process.env.REACT_APP_GOOGLE_MAP_ID} >
-            <div className='container bg-white p-3' style={{ position: 'absolute', top: 10, left: 370, width: "570px" }}>
+            <div className='container bg-white p-3' style={{ position: 'absolute', top: 10, left: 10, width: "900px" }}>
               <div className='row'>
-                <div className="col-sm-4">
+                <div className="col">
                   <AutoCompletes onPlaceSelect={setSelectedSourcePlace} />
                   <label for="distance" className="form-label mt-1">Distance: {routeSummary?.[0]?.legs?.[0]?.distance?.text}</label>
                 </div>
-                <div className="col-sm-4">
+                <div className="col">
                   <AutoCompletes onPlaceSelect={setSelectedDestinationPlace} />
                   <label for="timecal" className="form-label mt-1">Time: {routeSummary?.[0]?.legs?.[0]?.duration?.text}</label>
                 </div>
-                <div className='col-sm-4'>
+                <div className="col">
+                  <Select isSearchable={false} options={travelModeOptions} defaultValue="DRIVING" onChange={handleTravelMode} />
+                </div>
+                {(selectedTravelMode && selectedTravelMode.value === "TRANSIT") ? <div className="col">
+                  <Select isSearchable={false} isClearable={true} isMulti options={transitOptions} onChange={handleTransitMode} />
+                </div> : null}
+                <div className='col'>
                   <button type="button" className="btn btn-primary" onClick={handleRoute}>Get Route</button>
                 </div>
               </div>
             </div>
-            {(selectedSourcePlace && selectedDestinationPlace && startEndDirection && startEndDirection.origin && startEndDirection.destination) ? <Directions origin={startEndDirection.origin} destination={startEndDirection.destination} setRouteSummary={setRouteSummary} /> : null}
+
+            {/* {(startEndDirection && startEndDirection.origin && startEndDirection.destination) ? <div className='bg-white' style={{ position: 'absolute', top: 20, left: 970, width: "300px", height: "530px", overflow: "auto" }}>
+              {<p className='text-center mt-2'><strong>{routeSummary?.[0]?.legs?.[0].steps && routeSummary?.[0]?.legs?.[0].steps?.[0]?.travel_mode}</strong></p>}
+              {routeSummary?.[0]?.legs?.[0].steps && routeSummary?.[0]?.legs?.[0].steps.map((value, index) => {
+                return (
+                  <div className='p-2 bg-light border' key={index}>
+                    <span className='me-2 text-warning'>
+                      {index + 1}
+                    </span>
+                    <span dangerouslySetInnerHTML={{ __html: value.instructions }} />
+                    <p className='text-center mt-1'>{value.distance.text + ": "}{value.duration.text}</p>
+                  </div>
+                )
+              })}
+            </div> : null} */}
+
+
+            {(startEndDirection && startEndDirection.origin && startEndDirection.destination) ? <Directions origin={startEndDirection.origin} destination={startEndDirection.destination} setRouteSummary={setRouteSummary} /> : null}
+
+            {/* 
+            {(selectedSourcePlace && selectedDestinationPlace && startEndDirection && startEndDirection.origin && startEndDirection.destination) ? <div className='bg-white' style={{ position: 'absolute', top: 20, left: 970, width: "300px", height: "530px", overflow: "auto" }}>
+              {<p className='text-center mt-2'><strong>{routeSummary?.[0]?.legs?.[0].steps && routeSummary?.[0]?.legs?.[0].steps?.[0]?.travel_mode}</strong></p>}
+              {routeSummary?.[0]?.legs?.[0].steps && routeSummary?.[0]?.legs?.[0].steps.map((value, index) => {
+                return (
+                  <div className='p-2 bg-light border' key={index}>
+                    <span className='me-2 text-warning'>
+                      {index + 1}
+                    </span>
+                    <span dangerouslySetInnerHTML={{ __html: value.instructions }} />
+                    <p className='text-center mt-1'>{value.distance.text + ": "}{value.duration.text}</p>
+                  </div>
+                )
+              })}
+            </div> : null}
+
+
+            {(selectedSourcePlace && selectedDestinationPlace && startEndDirection && startEndDirection.origin && startEndDirection.destination) ? <Directions origin={startEndDirection.origin} destination={startEndDirection.destination} setRouteSummary={setRouteSummary} /> : null} */}
           </Map>
         </div>
       </APIProvider>
--- src/Google/AutoCompleteRoute/Directions.js
+++ src/Google/AutoCompleteRoute/Directions.js
@@ -1,22 +1,26 @@
 import { useMap, useMapsLibrary } from "@vis.gl/react-google-maps";
 import { useEffect, useState } from "react";
 
-function Directions({origin, destination, setRouteSummary}) {
+function Directions({ origin, destination, setRouteSummary }) {
     const map = useMap();
     const routesLibrary = useMapsLibrary('routes');
     const [directionsService, setDirectionsService] = useState();
     const [directionsRenderer, setDirectionsRenderer] = useState();
     const [routes, setRoutes] = useState([]);
+    console.log('routes:', routes)
     const [routeIndex, setRouteIndex] = useState(0);
     const selected = routes[routeIndex];
 
     const leg = selected?.legs[0];
-    
+
+
+    const [selectRoute, setSelectRoute] = useState({})
+    console.log('DirectionselectRoute:', selectRoute)
+
     // 1. Initialize directions service and renderer
-    
+
     useEffect(() => {
         if (!routesLibrary || !map) return;
-        console.log('1.Initialize directions service and renderer:')
         setDirectionsService(new routesLibrary.DirectionsService());
         setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
     }, [routesLibrary, map, origin, destination]);
@@ -24,16 +28,30 @@ function Directions({origin, destination, setRouteSummary}) {
     // 2. Use directions service
     useEffect(() => {
         if (!directionsService || !directionsRenderer) return;
-        console.log('2.Use directions service:')
         directionsService
             .route({
                 origin: origin,
                 destination: destination,
-                travelMode: "DRIVING",
-                provideRouteAlternatives: true
+                provideRouteAlternatives: true,
+                // travelMode: 'TRANSIT',
+                // transitOptions: {
+                //     departureTime: new Date(/* now, or future date */),
+                //     modes: ['BUS'],
+                //     routingPreference: 'FEWER_TRANSFERS'
+                // },
+                travelMode: 'DRIVING',
+                // drivingOptions: {
+                //     departureTime: new Date(/* now, or future date */),
+                //     trafficModel: 'pessimistic'
+                // },
             })
             .then(response => {
                 directionsRenderer.setDirections(response);
+                directionsRenderer.setOptions({
+                    polylineOptions: {
+                        strokeColor: 'blue'
+                    }
+                });
                 setRoutes(response.routes);
                 setRouteSummary(response.routes)
             });
@@ -44,24 +62,58 @@ function Directions({origin, destination, setRouteSummary}) {
     //3. Update direction route
     useEffect(() => {
         if (!directionsRenderer) return;
-    console.log('3.Update direction route:')
+
         directionsRenderer.setRouteIndex(routeIndex);
     }, [routeIndex, directionsRenderer, origin, destination]);
 
-    console.log("Directions before leg")
+    const handleRoute = (index, route) => {
+        setRouteIndex(index)
+        setSelectRoute(route)
+    }
+
     if (!leg) return null;
-    console.log("Directions after leg")
 
     return (
-        <ul>
-            {routes.map((route, index) => (
-                <li key={route.summary}>
-                    <button onClick={() => setRouteIndex(index)}>
-                        {route.summary}
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <div className='bg-white' style={{ position: 'absolute', top: 20, left: 990, width: "300px", height: "530px", overflow: "auto" }}>
+                {<p className='text-center mt-2'><strong>{routes?.[0]?.legs?.[0].steps && routes?.[0]?.legs?.[0].steps?.[0]?.travel_mode}</strong></p>}
+                {selectRoute &&
+    Object.keys(selectRoute).length === 0 &&
+    selectRoute.constructor === Object ? <ul className="bg-white p-2">
+                    {routes.map((route, index) => (
+                        <li key={index} className="bg-light mb-1">
+                            <div role="button" onClick={() => handleRoute(index, route)} className="d-flex justify-content-between p-2">
+                                {/* - {route.summary} */}
+                                <div className="mt-4 me-2"> <p>{route.legs?.[0].arrival_time?.text}{" "}{routes?.[0]?.legs?.[0]?.departure_time?.text}</p></div>
+                                <div> <p>{route.legs?.[0]?.distance?.text}</p>
+                                    <p>{routes?.[0]?.legs?.[0]?.duration?.text}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul> : null}
+
+                {selectRoute ? <button className="btn btn-light" onClick={() => setSelectRoute({})}>Back</button> : null}
+
+                {selectRoute.legs?.[0].steps?.map((value, index)=>{
+                    return(
+                        <div key={index}>{value?.distance?.text}</div>
+                    )
+                })}
+
+                {/* {routes?.[0]?.legs?.[0].steps && routes?.[0]?.legs?.[0].steps.map((value, index) => {
+                    return (
+                        <div className='p-2 bg-light border' key={index}>
+                            <span className='me-2 text-warning'>
+                                {index + 1}
+                            </span>
+                            <span dangerouslySetInnerHTML={{ __html: value.instructions }} />
+                            <p className='text-center mt-1'>{value.distance.text + ": "}{value.duration.text}</p>
+                        </div>
+                    )
+                })} */}
+            </div>
+        </div>
     );
 }
 
