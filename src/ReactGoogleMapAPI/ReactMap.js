92402490c798a43cbfe084af6e8a10fafba1b150
deepakmp444 <deepakmp444@gmail.com>
2024-02-26 00:24:53 +0530

    route completed

--- src/ReactGoogleMapAPI/ReactMap.js
+++ src/ReactGoogleMapAPI/ReactMap.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useJsApiLoader, GoogleMap, Autocomplete } from "@react-google-maps/api";
+import React, { useState, useRef } from 'react';
+import { useJsApiLoader, GoogleMap, Autocomplete, DirectionsRenderer } from "@react-google-maps/api";
 
 const center = { lat: 20.59, lng: 78.96 };
 
@@ -9,36 +9,67 @@ function ReactMap() {
         libraries: ['places']
     });
 
+    const [directionResponse, setDirectionResponse] = useState(null);
+    const [distance, setDistance] = useState(null);
+
+    const originRef = useRef();
+    const destinationRef = useRef();
+
     if (!isLoaded) return <p>Loading</p>;
 
+    const directionCalulate = async () => {
+        if (originRef.current.value === "" || destinationRef.current.value === "") {
+            return;
+        }
+
+        // eslint-disable-next-line no-undef
+        const directionService = new google.maps.DirectionsService();
+        const results = await directionService.route({
+            origin: originRef.current.value,
+            destination: destinationRef.current.value,
+            // eslint-disable-next-line no-undef
+            travelMode: google.maps.TravelMode.DRIVING
+        });
+
+        setDirectionResponse(results);
+        setDistance(results.routes[0].legs[0].distance.text);
+    };
+
+    // Options for the DirectionsRenderer to change polyline color
+    const directionsRendererOptions = {
+        polylineOptions: {
+            strokeColor: "#ff0000" // Change the color to red (you can specify any valid CSS color)
+        }
+    };
+
     return (
-        <div>
-            <h1>ReactMap</h1>
-            <div style={{ position: 'relative', height: '85vh' }}>
-                <GoogleMap center={center} zoom={10} mapContainerStyle={{ width: "100%", height: "80%" }} options={{ zoomControl: false, }}>
-                    <div className="card">
-                        <div className="bg-white p-3">
-                            <div className='container'>
-                                <div className='row'>
-                                    <div className='col-sm-4 offset-sm-1'>
-                                        <Autocomplete>
-                                            <input type="text" placeholder="Origin" />
-                                        </Autocomplete>
-                                    </div>
-                                    <div className='col-sm-4'>
-                                        <Autocomplete>
-                                            <input type="text" placeholder="Destination" />
-                                        </Autocomplete>
-                                    </div>
-                                    <div className='col-sm-2'>
-                                        <button className="btn btn-primary" >Get Route</button>
-                                    </div>
+        <div style={{ position: 'relative', height: '85vh' }}>
+            <GoogleMap center={center} zoom={10} mapContainerStyle={{ width: "100%", height: "80%" }} options={{ zoomControl: false }}>
+                <div className="card">
+                    <div className="bg-white p-3">
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='col-sm-4 offset-sm-1'>
+                                    <Autocomplete>
+                                        <input type="text" placeholder="Origin" ref={originRef} />
+                                    </Autocomplete>
+                                    <p>distance: {distance}</p>
+                                </div>
+                                <div className='col-sm-4'>
+                                    <Autocomplete>
+                                        <input type="text" placeholder="Destination" ref={destinationRef} />
+                                    </Autocomplete>
+                                </div>
+                                <div className='col-sm-2'>
+                                    <button className="btn btn-primary" onClick={directionCalulate}>Get Route</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </GoogleMap>
-            </div>
+                </div>
+
+                {directionResponse && <DirectionsRenderer directions={directionResponse} options={directionsRendererOptions} />}
+            </GoogleMap>
         </div>
     );
 }
